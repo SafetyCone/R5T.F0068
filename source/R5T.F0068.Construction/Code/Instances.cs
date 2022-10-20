7133560f876d0be5be6842605cfa3adc5ef8149f
davidcoats <david@getsafetycone.com>
2022-10-20 15:56:48 -0700

    Initial commit.

---
+++ source/R5T.F0068.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.F0068.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.F0068/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0068
+{
+	/// <summary>
+	/// Code file operations (CodeFileOperations) functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
