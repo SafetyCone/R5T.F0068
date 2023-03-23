0ee9f7fd121a9d353b8d2f043223a608ce2e928b
davidcoats <david@getsafetycone.com>
2023-03-23 02:28:56 -0700

    Interim changes.

--- source/R5T.F0068/Code/Instances.cs
+++ source/R5T.F0068/Code/Instances.cs
@@ -1,32 +1,25 @@
 using System;
 
-using R5T.F0000;
-using R5T.F0002;
-using R5T.F0018;
-using R5T.F0024;
-using R5T.Z0006;
-using R5T.Z0015;
-
 
 namespace R5T.F0068
 {
     public static class Instances
     {
-        public static IAssemblyOperator AssemblyOperator { get; } = F0000.AssemblyOperator.Instance;
-        public static IDirectoryNames DirectoryNames { get; } = F0068.DirectoryNames.Instance;
-        public static IDirectoryPaths DirectoryPaths { get; } = F0068.DirectoryPaths.Instance;
-        public static IEnumerableOperator EnumerableOperator { get; } = F0000.EnumerableOperator.Instance;
-        public static IFileNames FileNames { get; } = F0068.FileNames.Instance;
-        public static IFileOperator FileOperator { get; } = F0000.FileOperator.Instance;
-        public static IFilePaths FilePaths { get; } = Z0015.FilePaths.Instance;
-        public static F0000.IFileSystemOperator FileSystemOperator { get; } = F0000.FileSystemOperator.Instance;
-        public static INamespacedTypeNames NamespacedTypeNames { get; } = Z0006.NamespacedTypeNames.Instance;
-        public static INamespaces Namespaces { get; } = F0068.Namespaces.Instance;
-        public static IOperations Operations { get; } = F0068.Operations.Instance;
-        public static F0002.IPathOperator PathOperator { get; } = F0002.PathOperator.Instance;
-        public static IProjectPathsOperator ProjectPathsOperator { get; } = F0068.ProjectPathsOperator.Instance;
-        public static IReflectionOperator ReflectionOperator { get; } = F0018.ReflectionOperator.Instance;
-        public static ISolutionFileOperator SolutionFileOperator { get; } = F0024.SolutionFileOperator.Instance;
-        public static F0018.ITypeOperator TypeOperator { get; } = F0018.TypeOperator.Instance;
+        public static F0000.IAssemblyOperator AssemblyOperator => F0000.AssemblyOperator.Instance;
+        public static IDirectoryNames DirectoryNames => F0068.DirectoryNames.Instance;
+        public static IDirectoryPaths DirectoryPaths => F0068.DirectoryPaths.Instance;
+        public static F0000.IEnumerableOperator EnumerableOperator => F0000.EnumerableOperator.Instance;
+        public static IFileNames FileNames => F0068.FileNames.Instance;
+        public static F0000.IFileOperator FileOperator => F0000.FileOperator.Instance;
+        public static Z0015.IFilePaths FilePaths => Z0015.FilePaths.Instance;
+        public static F0000.IFileSystemOperator FileSystemOperator => F0000.FileSystemOperator.Instance;
+        public static Z0006.INamespacedTypeNames NamespacedTypeNames => Z0006.NamespacedTypeNames.Instance;
+        public static INamespaces Namespaces => F0068.Namespaces.Instance;
+        public static IOperations Operations => F0068.Operations.Instance;
+        public static F0002.IPathOperator PathOperator => F0002.PathOperator.Instance;
+        public static IProjectPathsOperator ProjectPathsOperator => F0068.ProjectPathsOperator.Instance;
+        public static F0018.IReflectionOperator ReflectionOperator => F0018.ReflectionOperator.Instance;
+        public static F0024.ISolutionFileOperator SolutionFileOperator => F0024.SolutionFileOperator.Instance;
+        public static F0018.ITypeOperator TypeOperator => F0018.TypeOperator.Instance;
     }
 }
